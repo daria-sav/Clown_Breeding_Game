9e6adf00f2e205ad4a121daf8f7a9bf3683db129
dariasav <daria.savtsenko@gmail.com>
2024-05-02 19:41:04 +0300

    creating buttons

--- src/main/java/org/example/ourgame/GameGUI.java
+++ src/main/java/org/example/ourgame/GameGUI.java
@@ -1,6 +1,7 @@
 package org.example.ourgame;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -18,33 +19,60 @@ public class GameGUI extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        // Фон игры
-        Image backgroundImage = new Image("background.jpg");
-        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+    public void start(Stage pealava) {
 
         // Создание корневого контейнера
-        StackPane root = new StackPane();
-        root.setBackground(new Background(background));
+        StackPane juur = new StackPane();
+
+        // Фон игры
+        Image tagaplaaniPilt = new Image("background.jpg");
+        BackgroundImage tagaplaan = new BackgroundImage(tagaplaaniPilt, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
+        juur.setBackground(new Background(tagaplaan));
 
         // Создание кнопки-картинки
-        Image buttonImage = new Image("shopButton.jpg");
-        ImageView shopButton = new ImageView(buttonImage);
-        shopButton.setPreserveRatio(true);
-        shopButton.setFitWidth(100);
-
+        ImageView shopButton = createButton("shopButton.jpg");
         // Обработка события при клике на кнопку
         shopButton.setOnMouseClicked(event -> {
-            System.out.println("Nupp oli vajutatud!");
+            System.out.println("Nupp pood oli vajutatud!");
         });
 
-        // Добавление кнопки на корневой контейнер
-        root.getChildren().add(shopButton);
+        ImageView worldSwitchButton = createButton("worldButton.jpg");
+        worldSwitchButton.setOnMouseClicked(event -> {
+            System.out.println("Nupp maailm oli vajutatud!");
+        });
+
+        ImageView galleryButton = createButton("clownButton.jpg");
+        galleryButton.setOnMouseClicked(event -> {
+            System.out.println("Nupp galerii oli vajutatud!");
+        });
+
+        // Размещение кнопок в разных углах экрана
+        VBox leftTopBox = new VBox(shopButton);
+        VBox.setVgrow(shopButton, Priority.ALWAYS);
+        leftTopBox.setAlignment(Pos.TOP_LEFT);
+
+        HBox rightTopBox = new HBox(worldSwitchButton);
+        HBox.setHgrow(worldSwitchButton, Priority.ALWAYS);
+        rightTopBox.setAlignment(Pos.TOP_RIGHT);
+
+        VBox rightBottomBox = new VBox(galleryButton);
+        VBox.setVgrow(galleryButton, Priority.ALWAYS);
+        rightBottomBox.setAlignment(Pos.BOTTOM_RIGHT);
+
+        juur.getChildren().addAll(leftTopBox, rightTopBox, rightBottomBox);
 
         // Создание сцены и отображение главного окна
-        Scene scene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Breeding Clowns Game");
-        primaryStage.show();
+        Scene stseen = new Scene(juur, SCREEN_WIDTH, SCREEN_HEIGHT);
+        pealava.setScene(stseen);
+        pealava.setTitle("Breeding Clowns Game");
+        pealava.show();
+    }
+
+    private ImageView createButton(String pilt) {
+        Image nuppuPilt = new Image(pilt);
+        ImageView nupp = new ImageView(nuppuPilt);
+        nupp.setPreserveRatio(true);
+        nupp.setFitWidth(100);
+        return nupp;
     }
 }
